1c51db42690b228ccf118fbf37d2f7f014ac68c8
gromkoviec <pankrzysztof@o2.pl>
2018-08-12 12:10:35 +0200

    first

---
+++ script/main.js
@@ -0,0 +1,40 @@
+var list = document.querySelector('ul'); // odnosi sie do sekcji <ul>
+var input = document.querySelector('input'); // odnosi sie do sekcji <input type="text" name="item" id="item">
+var button = document.querySelector('button'); // <button>
+
+      button.onclick = function() {
+var myItem = input.value;
+        input.value = '';
+          
+var listItem = document.createElement('li');
+var listText = document.createElement('span');
+var listBtn = document.createElement('button');
+var listBtn2 = document.createElement('button');
+var listBtn3 = document.createElement('button'); 
+var edtPlc = document.createElement('imput');          
+          
+listItem.appendChild(listText);  // appendChild dodaje dotakowy węzeł do struktury na końcu 
+listText.textContent = myItem;
+listItem.appendChild(listBtn);
+listBtn.textContent = 'Delete';
+listItem.appendChild(listBtn2);
+listBtn2.textContent = 'Done';
+listItem.appendChild(listBtn3);
+listBtn3.textContent = 'Edit';
+list.appendChild(listItem);
+          
+listBtn.onclick = function(e) {
+  list.removeChild(listItem);
+}
+listBtn2.onclick = function(e) {
+  var done = ' Done ';
+  listItem.removeChild(listBtn2);  //var doneColor = done.fontcolor('green');    
+ listText.textContent = myItem + done;
+}
+listBtn3.onclick = function(e) {
+    edtPlc.appendChild('imput')    
+    var newCont = '.....';   
+    listText.textContent = myItem.replace(myItem,newCont);
+}
+input.focus(); // przekierowanie na okno dodawania zadań
+}
